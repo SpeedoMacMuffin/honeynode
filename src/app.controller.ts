6979aeacf8053da8a391b6ba4210bb01f7d8378e
SpeedoMacMuffin <66319208+SpeedoMacMuffin@users.noreply.github.com>
2022-05-26 14:33:59 +0200

    test for capturing request

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Request } from 'express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/*')
+  getHello(@Req() req: Request): any {
+    const { url, headers, ips } = req;
+    console.log({ url, headers, ips });
+    // get ip adress of request
+    const ip = ips[0];
+    // get url of request
+    const urlPath = url.split('/')[1];
+    console.log({ ip, urlPath });
+    return { url, headers, ips, ip, urlPath };
   }
 }
